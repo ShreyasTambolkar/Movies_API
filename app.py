d28be6aafcaee6976fcbd11fe67bcbe9d8e268ad
Shreyas Tambolkar <tambolkarshreyas@gmail.com>
2026-06-06 11:54:22 +0530

    Fixed Validation

--- app.py
+++ app.py
@@ -143,6 +143,9 @@ def update_movie(id):
     if "release_year" in data and (data["release_year"] < 1950 or data["release_year"] > 2026):
         conn.close()
         return jsonify({"message": "Please enter a valid release year!"}), 400
+    if "release_year" in data and (data["release_year"] == "release_year" ):
+        conn.close()
+        return jsonify({"message": "The current year cannot be same as release year"}), 400
 
     allowed_fields = ["title", "director", "genre", "release_year", "rating"]
     updates = {key: value for key, value in data.items() if key in allowed_fields}
