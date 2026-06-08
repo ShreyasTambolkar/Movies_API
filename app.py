2bb216aa8d4722f82f33ecbd1deb3020e16d9b0e
Shreyas Tambolkar <tambolkarshreyas@gmail.com>
2026-06-08 10:38:46 +0530

    Fixed APP

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, jsonify, request
 from database import get_connection, init_db
-from flask_cors import CORS
+
 
 app = Flask(__name__)
-CORS(app)   
+
 
 # ─── GET ALL MOVIES ──────────────────────────────────────────────
 @app.route("/movies", methods=["GET"])
